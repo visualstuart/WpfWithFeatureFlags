ac97e00a01d731c3d45a71b08b6358fa7fc5e4db
Stuart Celarier <stuart.celarier@softvision.com>
2022-04-13 15:08:34 -0700

    Initial project from wpf template - Added a button

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace WpfDotNet6WithFeatureFlags
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
