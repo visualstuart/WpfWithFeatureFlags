c855bb8c8591b57852fdeb68835f2ac8366128be
Stuart Celarier <stuart.celarier@softvision.com>
2022-04-14 12:18:55 -0700

    Bind visibility of a StackPanel to a property loaded from an Azure App Configuration feature flag.

---
+++ WpfDotNet6DataBinding/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace WpfDotNet6DataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string Greeting { get => "Hello"; set { } }
+        public Visibility GreetingVisibility { get => Visibility.Hidden; set { } }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
