1b94b61e565d256f630d3bb638f60d2dab762133
Stuart Celarier <stuart.celarier@softvision.com>
2022-04-14 12:19:29 -0700

    Bind visibility of a StackPanel to a property loaded from an Azure App Configuration feature flag.

---
+++ WpfDotNet6WithFeatureFlags/App.xaml.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.FeatureManagement;
+using System;
+using System.Reflection;
+using System.Windows;
+
+namespace WpfDotNet6WithFeatureFlags
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly IHost host;
+
+        public App()
+        {
+            var builder = new HostBuilder();
+
+            // configure configuration
+            builder
+                .UseEnvironment("Development")      // cheating...
+                .ConfigureAppConfiguration((hostingContext, configBuilder) =>
+                {
+                    LoadUserSecrets(hostingContext, configBuilder);
+
+                    var connectionString =
+                        configBuilder.Build().GetConnectionString("AppConfig");
+
+                    configBuilder
+                        .AddAzureAppConfiguration(options => options
+                            .Connect(connectionString)
+                            .UseFeatureFlags());
+                });
+
+            // configure services
+            builder
+                .ConfigureServices((context, services) =>
+                    {
+                        services
+                            .AddAzureAppConfiguration()
+                            .AddFeatureManagement();
+
+                        services.AddSingleton<MainWindow>();
+                    });
+
+            // build the host
+            host = builder.Build();
+        }
+
+        /// <summary>
+        /// Load user secrets if executing in a development environment.
+        /// </summary>
+        /// <param name="hostingContext"></param>
+        /// <param name="configBuilder"></param>
+        /// <remarks>
+        /// Following the example in CreateDefaultBuilder(string[] args) in WebHost
+        /// https://github.com/dotnet/aspnetcore/blob/f08285d0b6918fbb2b485d97f4e411dc9ea9a94f/src/DefaultBuilder/src/WebHost.cs
+        /// </remarks>
+        private static void LoadUserSecrets(HostBuilderContext hostingContext, IConfigurationBuilder configBuilder)
+        {
+            var environment = hostingContext.HostingEnvironment;
+            if (environment.IsDevelopment())
+            {
+                if (!string.IsNullOrEmpty(environment.ApplicationName))
+                {
+                    var appAssembly = Assembly.Load(new AssemblyName(environment.ApplicationName));
+                    if (appAssembly != null)
+                    {
+                        configBuilder.AddUserSecrets(appAssembly, optional: true);
+                    }
+                }
+            }
+        }
+
+        private async void Application_Startup(object sender, StartupEventArgs e)
+        {
+            await host.StartAsync();
+
+            host.Services.GetService<MainWindow>()?.Show();
+        }
+
+        private async void Application_Exit(object sender, ExitEventArgs e)
+        {
+            await host.StopAsync(TimeSpan.FromSeconds(5));
+        }
+    }
+}
---
+++ WpfDotNet6WithFeatureFlags/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.FeatureManagement;
+using System.Windows;
+
+namespace WpfDotNet6WithFeatureFlags
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly IFeatureManager featureManager;
+
+        public Visibility FeatureXVisibility { get; set; }
+
+        public MainWindow(IFeatureManagerSnapshot featureManager)
+        {
+            this.featureManager = featureManager;
+            FeatureXVisibility = ConvertToVisibility(featureManager.IsEnabledAsync("FeatureX").Result); 
+
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private static Visibility ConvertToVisibility(bool isFeatureEnabled) =>
+            isFeatureEnabled ? Visibility.Visible : Visibility.Hidden;
+    }
+}
